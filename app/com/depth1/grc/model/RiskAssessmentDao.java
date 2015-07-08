949b3c77fe1b3f352f3ddc73345ec9fae9a60fd2
CharlieL7 <charlielin7@gmail.com>
2015-07-08 14:24:38 -0400

    Non compiled Backend Removal, add UI
    Removed the backend code that could not yet compile. Ye and Sameer must
    work on it so that it atleast compiles so we can test the UI. Put future
    non compiling code in a pull request to be checked.
    
    Added some UI and changed method name findAll() to listRiskAssessment().

--- app/com/depth1/grc/model/RiskAssessmentDao.java
+++ app/com/depth1/grc/model/RiskAssessmentDao.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public interface RiskAssessmentDao {
 
-    void createRiskAssessment(RiskAssessment riskAssessment) throws DaoException;
-    boolean updateRiskAssessment(final RiskAssessment riskAssessment) throws DaoException;
-    boolean deleteRiskAssessment(int riskAssessmentId) throws DaoException;
-    void viewRiskAssessment(int riskAssessmentId) throws DaoException;
-    RiskAssessment findRiskAssessment(int riskAssessmentId) throws DaoException;
-    List<RiskAssessment> findAll() throws DaoException; //not type void, some list of riskAssessments
+	public void createRiskAssessment(RiskAssessment riskAssessment) throws DaoException;
+	public boolean updateRiskAssessment() throws DaoException;
+	public boolean deleteRiskAssessment() throws DaoException;
+	public void viewRiskAssessment() throws DaoException;
+	public RiskAssessment findRiskAssessment() throws DaoException;
+	public List<RiskAssessment> listRiskAssessment(); 
 }
