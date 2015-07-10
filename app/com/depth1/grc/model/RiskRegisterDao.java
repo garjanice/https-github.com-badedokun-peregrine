4f32c13829dba39d220e5376b355944f944966e4
aneeshmunnam <munnamaneesh2@gmail.com>
2015-07-10 15:15:55 -0500

    Update RiskRegisterDao.java

--- app/com/depth1/grc/model/RiskRegisterDao.java
+++ app/com/depth1/grc/model/RiskRegisterDao.java
@@ -8,5 +8,15 @@ package com.depth1.grc.model;
  *
  */
 public interface RiskRegisterDao {
+	
+	public void createRiskRegister(Register register) throws DaoException;
+	
+	public boolean updateRiskRegister() throws DaoException;
+	
+	//public boolean viewRiskRegister() throws DaoException;
+	
+	public List<Register> listRegister throws DaoException;
+	
+	public Register findRegister(int riskId) throws DaoException; 
 
 }
