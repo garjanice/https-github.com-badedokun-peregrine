b8dec1e10f3f48213a1728ea2ad1c5aae11b2d38
aneeshmunnam <munnamaneesh2@gmail.com>
2015-07-14 17:30:32 -0500

    Update Application.java

--- app/com/depth1/grc/controllers/Application.java
+++ app/com/depth1/grc/controllers/Application.java
@@ -1,99 +1,83 @@
 package com.depth1.grc.controllers;
 
-import java.util.List;
-
+import play.*;
+import play.mvc.*;
 import play.Logger;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.List;
+
 import com.datastax.driver.core.ResultSetFuture;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.DriverException;
 import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.depth1.grc.db.util.CassandraPoolImpl;
 import com.depth1.grc.model.DaoException;
 import com.depth1.grc.model.DaoFactory;
-import com.depth1.grc.model.RiskAssessment;
-import com.depth1.grc.model.RiskRegister;
-import com.depth1.grc.model.RiskAssessmentDao;
+import com.depth1.grc.model.RiskRegisterDao;
+import com.depth1.grc.model.Register;
 import com.depth1.grc.model.Tenant;
 import com.depth1.grc.model.TenantDao;
-import com.depth1.grc.views.html.createRA;
-import com.depth1.grc.views.html.frontRA;
+import com.depth1.grc.views.html.createregister;
 import com.depth1.grc.views.html.index;
-import com.depth1.grc.views.html.updateRA;
-import com.depth1.grc.views.html.viewRA;
-import com.depth1.grc.views.html.createRR;
 import com.depth1.grc.views.html.frontRR;
 
 
 public class Application extends Controller {
-
-	// create the required DAO Factory
-	static DaoFactory cassandraFactory = DaoFactory
-			.getDaoFactory(DaoFactory.CASSANDRA);
-	final static Form<RiskAssessment> rAForm = Form.form(RiskAssessment.class);
-	//final static Form<RiskRegister> rRForm = Form.form(RiskRegister.class);
-	static List<RiskAssessment> riskAssessments;
-	static RiskAssessment selectedRA;
 	
-	static List<RiskRegister> riskRegisters;
-	final static Form<RiskRegister> rRForm = Form.form(RiskRegister.class);
+	// create the required DAO Factory
+	static DaoFactory cassandraFactory = DaoFactory.getDaoFactory(DaoFactory.CASSANDRA);
+	final static Form<Register> rRForm=Form.form(Register.class);
+	static List<Register> registers;
 
-	public Result index() {
-		// test connection to the cassandra cluster
+ public Result index() {
+    	// test connection to the cassandra cluster
+    	//CassandraPoolImpl con = new CassandraPoolImpl();
+    	//Session session = con.create();
+    	//System.out.println();
+    	//System.out.println("Trying to get the information in DataBase");
+    	//printState(session);
+    	//session.close();
+   	    return ok(index.render("Database Created Successfully"));
+    }
+    
+    
+    /**
+     * This method is used as a client to test getting data from the Cassandra database
+     * It displays data it reads from the database on the console
+     * @param session The established session to the cluster
+     * @return Returns a result in the future in an non-blocking fashion
+     */
+  //public ResultSetFuture getRegister(Session session) {    	
+    //	Statement query = QueryBuilder.select().all().from("grc", "riskregister");
+    	//return session.executeAsync(query);
+   //}
 
-		/*
-		 * CassandraPoolImpl con = new CassandraPoolImpl(); Session session =
-		 * con.create(); System.out.println();
-		 * System.out.println("Test client to display state");
-		 * System.out.println("============================");
-		 * printState(session); session.close();
-		 */
-		
-		//gets the list of previous RA, this code will be moved from the index to RA page method later
+    /**
+     * This method prints data retrieved from the Cassandra database
+     * It displays data it reads from the database on the console
+     * @param session The established session to the cluster
+     * 
+     */
+   
+    //public void printState(Session session) {
+      //  ResultSetFuture result = getRegister(session);
+       // for (Row row : result.getUninterruptibly()) {
+    	//System.out.printf( "%s: %s  %s\n",
+    		//row.getString("id"),
+    		//row.getString("me"),
+    	//	row.getString("long_name"));
+    	//}
+    	//session.close();
 
-		return ok(index.render()); 
-	}
-
+    //}
+    
 	/**
-	 * This method is used as a client to test getting data from the Cassandra
-	 * database It displays data it reads from the database on the console
-	 * 
-	 * @param session
-	 *            The established session to the cluster
-	 * @return Returns a result in the future in an non-blocking fashion
-	 */
-	public ResultSetFuture getState(Session session) {
-		Statement query = QueryBuilder.select().all().from("member", "state");
-		return session.executeAsync(query);
-
-	}
-
-	/**
-	 * This method prints data retrieved from the Cassandra database It displays
-	 * data it reads from the database on the console
-	 * 
-	 * @param session
-	 *            The established session to the cluster
-	 * 
-	 */
-
-	public void printState(Session session) {
-		ResultSetFuture result = getState(session);
-		for (Row row : result.getUninterruptibly()) {
-			System.out.printf("%s: %s  %s\n", row.getString("country"),
-					row.getString("short_name"), row.getString("long_name"));
-		}
-		session.close();
-
-	}
-
-	/**
-	 * @param tenant
-	 *            The tenant to create
+	 * @param tenant The tenant to create
 	 * @return Result the result of the tenant creation
 	 */
 	public static Result createTenant(Tenant tenant) {
@@ -107,80 +91,41 @@ public class Application extends Controller {
 		return ok();
 	}
 
-	/**
-	 * @param RiskAssessment
-	 *            The RA criteria to create
-	 * @return Result the result of the RAC creation
-	 */
-	public Result addRiskAssessment() {
-		Form<RiskAssessment> filledRA = rAForm.bindFromRequest();
-		RiskAssessment criteria = filledRA.get();
-		System.out
-				.println("Here it is: " + criteria.getLikelihoodDescription());
+	public Result addRisk() {
+    	
+		Form<Register> risky=rRForm.bindFromRequest();
+    	if(risky.hasErrors()){
+    		flash("Error","Please Correct the Form");
+    		return badRequest(createregister.render(rRForm));
+    	}
+    	Register register=risky.get();
 		try {
-			RiskAssessmentDao riskAssessmentDao = cassandraFactory
-					.getRiskAssessmentDao();
-			riskAssessmentDao.createRiskAssessment(criteria);
-		} catch (DaoException e) {
-			Logger.error(
-					"Error occurred while creating risk assessment criteria ",
-					e);
+			 	RiskRegisterDao riskregisterDao = cassandraFactory.getRiskRegisterDao();
+			 	riskregisterDao.createRiskRegister(register);
+		} catch(DaoException e) {
+			Logger.error("Error occures while creating Risk Register", e);
 		}
-
-		return redirect("/riskAssessment");
+		System.out.println("The data is retrieved into the database successfully!");
+   	 return redirect("/register");
 	}
-	
-	public Result deleteRiskAssessment() {
-	
-		return TODO;
-	}
-	
-	public Result showFrontRAPage() {
-
-		try {
-			RiskAssessmentDao riskAssessmentDao = cassandraFactory
-					.getRiskAssessmentDao();
-			riskAssessments = riskAssessmentDao.listRiskAssessment();
-		} catch (DaoException e) {
-			Logger.error(
-					"Error occurred while creating risk assessment criteria ",
-					e);
-		}
-
-		return ok(frontRA.render(riskAssessments)); // change to main page
-	}
-
-	public Result showCreateRAPage() {
-
-		return ok(createRA.render(rAForm));
-	}
-
-	public Result showViewRAPage() {
-
-		return ok(viewRA.render(selectedRA));
-	}
-
-	public Result showUpdateRAPage() {
-
-		return ok(updateRA.render(selectedRA));
-	}
-
-	//remove this later
-	public Result showDeleteRAPage() {
-
-		return TODO;
-	}
-	
+    	
 	public Result showFrontRRPage() {
-
-		return ok(frontRR.render(riskRegisters));
+		
+		try {
+		 	RiskRegisterDao riskregisterDao = cassandraFactory.getRiskRegisterDao();
+		 	riskregisterDao.listRegister();
+	} catch(DaoException e) {
+		Logger.error("Error occures while creating Risk Register", e);
+	}		
+		return ok(frontRR.render(registers));
 	}
 	
 	public Result showCreateRRPage() {
 
-		return ok(createRR.render(rRForm));
+		return ok(createregister.render(rRForm));
 	}
 
+	
 	public Result showViewRRPage() {
 
 		return TODO;
@@ -191,24 +136,4 @@ public class Application extends Controller {
 		return TODO;
 	}
 
-	
-	public Result addRiskRegister(){
-		Form<Register> rR1Form=rRForm.bindFromRequest();
-    	if(rRForm1.hasErrors()){
-    		flash("Error","Please Correct the Form");
-    		return badRequest(index.render(rRForm1));
-    	Register register=rRForm1.get();
-    	}
-		try {
-			 	RiskRegisterDao riskregisterDao = cassandraFactory.getRiskRegisterDao();
-			 	riskregisterDao.createRiskRegister(register);
-		} catch(DaoException e) {
-			Logger.error("Error occures while creating Risk Register", e);
-		}
-   	 return ok(index.render("Risk is Registered Successfully"));
-	}
-	}
-	
-	
 }
-	
