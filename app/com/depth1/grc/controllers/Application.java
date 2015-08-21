8e7cf10b2c1500f17c2cd5310489e91fef1e35d8
poojapurushotham <poojapurushotham@gmail.com>
2015-08-21 12:55:05 -0700

    code to upload files to text area

--- app/com/depth1/grc/controllers/Application.java
+++ app/com/depth1/grc/controllers/Application.java
@@ -254,21 +254,6 @@ tart */
 		}
 		return redirect("/policy");
 	}
-/*	
-	public Result importPolicyBody() throws DaoException {
-		MultipartFormData body = request().body().asMultipartFormData();
-		MultipartFormData.FilePart picture = body.getFile("picture");
-		if (picture != null) {
-	        String fileName = picture.getFilename();
-	        String contentType = picture.getContentType();
-	        File file = picture.getFile();
-	        return ok("File uploaded");
-	    } else {
-	        flash("error", "Missing file");
-	        return badRequest();
-	    }
-	}
-*/
 	
 	private void savePolicyBodyDocument(String fileName, Field policyBody) {
 		try {
