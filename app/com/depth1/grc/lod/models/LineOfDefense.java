110e64911534550f435677d2d6ea52d0d052dd0c
Bisi <developers@acenonyx.com>
2016-01-26 07:17:05 -0500

    Move lod to its own package

---
+++ app/com/depth1/grc/lod/models/LineOfDefense.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.depth1.grc.lod.models;
+
+/**
+ * This class is the business object for creating, reading, updating, and deleting (CRUD)
+ * Line of Defense
+ * 
+ * @author Bisi Adedokun
+ *
+ */
+public class LineOfDefense {
+	private int lodId;
+	private String description;
+	private String lod;
+	
+	/**
+	 * @return the lodId
+	 */
+	public int getLodId() {
+		return lodId;
+	}
+	/**
+	 * @param lodId the lodId to set
+	 */
+	public void setLodId(int lodId) {
+		this.lodId = lodId;
+	}	
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @return the lod
+	 */
+	public String getLod() {
+		return lod;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @param lod the lod to set
+	 */
+	public void setLod(String lod) {
+		this.lod = lod;
+	}	
+
+}
